4f1c5ea718636660eef9742004912e2fb1447317
Nick Solonyy <nicksolony@gmail.com>
2020-02-11 03:49:57 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,16 @@
 # Create your haunted_houses migration here
+
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+  def change
+  create_table :haunted_houses do |t|
+    t.string :name
+    t.string :location
+    t.integer :number_of_costumes
+    t.integer :number_of_employees
+    t.boolean :in_business
+    t.string :opening_time
+    t.string :closing_time
+  end
+end
+
+end
